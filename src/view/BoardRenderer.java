4e8b7e12a86981ca2b2a5d8d654af14129d52222
Sylvester C. Reventhal Abelsen <cornelius.a.r@gmail.com>
2012-10-05 16:09:12 +0200

    Tilfoejet Pat2D matrix til BoardRenderer
    drawHex returnerer nu en Path2d.
    tilfoejet colorShape.

--- src/view/BoardRenderer.java
+++ src/view/BoardRenderer.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -18,16 +19,20 @@ public class BoardRenderer extends JPanel {
 	private ArrayList<ArrayList<Hex>> hexMatrix;
 	private int columns = 4;
 	private int rows = 5;
+	private ArrayList<ArrayList<Path2D>> allPath2DColumns = new ArrayList<ArrayList<Path2D>>();
+	private ArrayList<Path2D> columnPath2D;
+	private Graphics2D g2;
 	
 	
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
     board = new Board(new Coordinate(50, 10), rows, columns, 50);
     hexMatrix = board.getHexMatrix();
-    Graphics2D g2 = (Graphics2D)g;
+    g2 = (Graphics2D)g;
     for(int column = 0; column < columns; column++) {
     	ArrayList<Hex> currentColumn = hexMatrix.get(column);
     	System.out.println("current column size is: " + Integer.toString(currentColumn.size()));
+    	columnPath2D = new ArrayList<Path2D>();
     	for(int row = 0; row < rows; row++) {
     		Hex hex = currentColumn.get(row);
     		Coordinate pos = hex.getStartPosition();
@@ -36,12 +41,14 @@ public class BoardRenderer extends JPanel {
     		String cString = Integer.toString(column);
     		String rString = Integer.toString(row);
     		System.out.println("hex at (" + rString + ", " + cString + ") : " + posX + ", " + posY);
-    		drawHex(currentColumn.get(row), g2);
+    		Path2D hexPath = drawHex(currentColumn.get(row), g2);
+    		columnPath2D.add(row, hexPath);
     	}
+    	allPath2DColumns.add(columnPath2D);
     }
   }
   
-  public void drawHex(Hex hexIn, Graphics2D g2) {
+  public Path2D drawHex(Hex hexIn, Graphics2D g2) {
 	  Path2D hexPath = new Path2D.Double();
 	  Coordinate startPosition = hexIn.getStartPosition();
 	    hexPath.moveTo(startPosition.getX(), startPosition.getY());
@@ -52,8 +59,20 @@ public class BoardRenderer extends JPanel {
 	    }
 	    hexPath.closePath();
 	    g2.draw(hexPath);
+	    return hexPath;
 	  
   }
+  
+  public void colorShape(Coordinate position, Color color) {
+	  Double col = position.getX();
+	  Double row = position.getY();
+	  ArrayList<Path2D> path2DArray = allPath2DColumns.get(col.intValue());
+	  Path2D hexShape = path2DArray.get(row.intValue());
+	  g2.setColor(Color.red);
+	  g2.fill(hexShape);
+	  
+  }
+  
   public static void main(String[] args) {
     JFrame frame = new JFrame();
     frame.setTitle("DrawPoly");
