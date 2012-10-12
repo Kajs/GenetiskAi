a4817525650d5ae4257150ce25ebd71a0408f3c4
OleO <placeholder@mail.com>
2012-10-12 16:38:32 +0200

    Ændring af AI til class. War og Wiz extender bare denne classe

--- src/model/Ai.java
+++ src/model/Ai.java
@@ -1,27 +1,120 @@
 package model;
 
+import static java.lang.Math.abs;
+
 import java.awt.Color;
 import java.util.ArrayList;
 
+import control.Controller;
+
 import model.Ai;
 import model.Coordinate;
-import model.OffensiveAction;
 
-public interface Ai {
+public class Ai {
 	
-	public Coordinate moveAction(ArrayList<Ai> enemies);
+	private Color color;
+	public Coordinate position;
+    private int hp;
+    public int stunned;
 	
-	public Coordinate getPosition();
+	public Coordinate moveAction(ArrayList<Ai> enemies) {		
+		return nearestEnemy(enemies);
+	}
 	
-	public Color getColor();
+	public Coordinate getPosition() {
+		return position;
+	}
 	
-	public boolean setHp(int newHp);
+	public Color getColor() {
+		return color;
+	}
 	
-	public int getHp();
+	public boolean setHp(int newHp) {
+		hp = newHp;
+		if (hp > 0) {
+			return true;
+		}
+		else return false;
+	}
+
+    public int getHp() {
+    	return hp;
+    }
 	
-	public boolean getStunned();
+    public void setStunned(int stunRounds) {
+		stunned = stunRounds;
+	}
+
+	public boolean getStunned() {
+		if (stunned > 0) {
+			stunned = stunned - 1;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 	
-	public void setStunned(int stunRounds);
+	public Coordinate nearestEnemy(ArrayList<Ai> enemies) {
+    	if (enemies.isEmpty()) { return position; }
+    	
+		Ai closest = enemies.get(0);
+		int fX = position.getX();
+		int fY = position.getY();
+		int eX = closest.getPosition().getX();
+		int eY = closest.getPosition().getY();
+		int dx = fX - eX;
+		int dy = fY - eY;
+		
+    	for (Ai enemy : enemies) {
+			int newDx = fX - enemy.getPosition().getX();
+			int newDy = fY - enemy.getPosition().getY();
+		
+			if (abs(newDx) + abs(newDy) < abs(dx) + abs(dy)) {
+				closest = enemy;
+				dx = newDx;
+				dy = newDy;
+			}
+		}
+    	int x = abs(dx);
+    	int y = abs(dy);
+    	if (x + y < 1) { return position; }
+
+    	for (Coordinate coordinate : freeCoordinates(fX, fY)) {
+    		int newDx = abs(coordinate.getX() - closest.getPosition().getX());
+    		int newDy = abs(coordinate.getY() - closest.getPosition().getY());
+    		if (x + y > newDx + newDy) {
+    			x = newDx;
+    			y = newDy;
+    		    position = coordinate;
+    		}
+    	}
+    	return position;
+    }
 	
-	public Coordinate nearestEnemy(ArrayList<Ai> enemies);
+	private ArrayList<Coordinate> freeCoordinates(int x, int y) {  	
+    	ArrayList<Coordinate> candidates = new ArrayList<Coordinate>();
+    	Coordinate pos1 = new Coordinate(x + 1, y);
+    	if (Controller.isOccupied(pos1) == 0) { candidates.add(pos1); }
+    	Coordinate pos2 = new Coordinate(x - 1, y);
+    	if (Controller.isOccupied(pos2) == 0) { candidates.add(pos2); }
+    	Coordinate pos3 = new Coordinate(x, y + 1);
+    	if (Controller.isOccupied(pos3) == 0) { candidates.add(pos3); }
+    	Coordinate pos4 = new Coordinate(x, y - 1);
+    	if (Controller.isOccupied(pos4) == 0) { candidates.add(pos4); }
+    	
+    	if (y % 2 == 0) {
+    	    Coordinate pos5 = new Coordinate(x - 1, y - 1);
+    	    if (Controller.isOccupied(pos5) == 0) { candidates.add(pos5); }
+    	    Coordinate pos6 = new Coordinate(x - 1, y + 1);
+    	    if (Controller.isOccupied(pos6) == 0) { candidates.add(pos6); }
+    	}
+    	else {
+    		Coordinate pos5 = new Coordinate(x + 1, y + 1);
+    	    if (Controller.isOccupied(pos5) == 0) { candidates.add(pos5); }
+    	    Coordinate pos6 = new Coordinate(x + 1, y + 1);
+    	    if (Controller.isOccupied(pos6) == 0) { candidates.add(pos6); }
+    	}
+    	return candidates;
+    }
 }
